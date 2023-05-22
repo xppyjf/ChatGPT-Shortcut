44547da88c375f72b2d9a99c07f26322ec779515
xppyjf <76797941+xppyjf@users.noreply.github.com>
2023-05-23 02:36:36 +0800

    Update Navbar.zh.js

--- src/components/LocalizedNavbar/Navbar.zh.js
+++ src/components/LocalizedNavbar/Navbar.zh.js
@@ -7,37 +7,11 @@ const zhNavbar = {
   },
   items: [
     {
-      to: "https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html",
-      label: "ChatGPT Shortcut 原理说明",
+      to: "https://www.aibxw.com",
+      label: "AI三杯茶官网",
       position: "left",
     },
-    {
-      type: 'dropdown',
-      label: '其他工具',
-      position: 'left',
-      items: [
-        {
-          label: "Chrome 扩展版",
-          href: "https://chrome.google.com/webstore/detail/chatgpt-shortcut/blcgeoojgdpodnmnhfpohphdhfncblnj",
-        },
-        {
-          label: 'IMGPrompt',
-          href: 'https://prompt.newzone.top/',
-        },
-        {
-          label: '文字处理',
-          href: 'https://tools.newzone.top/',
-        },
-        {
-          label: '工具收藏',
-          href: 'https://nav.newzone.top',
-        },
-        {
-          label: 'Find on Product Hunt',
-          href: 'https://www.producthunt.com/posts/chatgpt-shortcut?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chatgpt&#0045;shortcut',
-        },
-      ],
-    },
+    
     {
       to: "https://xq7pd3qu17.feishu.cn/share/base/form/shrcnK7oaSPaAcj1YAHfy4jo0Jh",
       label: "反馈/添加提示词",
