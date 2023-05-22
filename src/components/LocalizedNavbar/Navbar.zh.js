eb1edb2a683b51ea18ca8ab8db0d17f55f64ac35
xppyjf <76797941+xppyjf@users.noreply.github.com>
2023-05-23 02:29:04 +0800

    Update Navbar.zh.js

--- src/components/LocalizedNavbar/Navbar.zh.js
+++ src/components/LocalizedNavbar/Navbar.zh.js
@@ -1,16 +1,37 @@
 const zhNavbar = {
   hideOnScroll: true,
-  title: "ChatGPT sc",
+  title: "ChatGPT SC",
   logo: {
-    alt: "ChatGPT sc",
+    alt: "ChatGPT Shortcuts",
     src: "img/logo.svg",
   },
   items: [
     {
-      to: "https://www.aibxw.com/",
-      label: "Ai三杯茶官网",
+      to: "https://newzone.top/posts/2023-02-27-chatgpt_shortcuts.html",
+      label: "ChatGPT Shortcut 原理说明",
       position: "left",
     },
+    {
+      type: 'dropdown',
+      label: '其他工具',
+      position: 'left',
+      items: [
+        {
+          label: "Chrome 扩展版",
+          href: "https://chrome.google.com/webstore/detail/chatgpt-shortcut/blcgeoojgdpodnmnhfpohphdhfncblnj",
+        },
+        {
+          label: 'IMGPrompt',
+          href: 'https://prompt.newzone.top/',
+        },
+        {
+          label: '文字处理',
+          href: 'https://tools.newzone.top/',
+        },
+        {
+          label: '工具收藏',
+          href: 'https://nav.newzone.top',
+        },
         {
           label: 'Find on Product Hunt',
           href: 'https://www.producthunt.com/posts/chatgpt-shortcut?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-chatgpt&#0045;shortcut',
